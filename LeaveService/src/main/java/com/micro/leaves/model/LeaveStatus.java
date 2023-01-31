f8ec3ea7069ef3592d6847cc6779feff107dabfe
sabir <sabir.hussain@convo.com>
2023-01-31 09:43:01 +0500

    Revert "employee and leave microservices with spring cloud gateway"
    This reverts commit 035c5135dd221856c382ba0daf832c247ce6d903.

--- LeaveService/src/main/java/com/micro/leaves/model/LeaveStatus.java
+++
@@ -1,6 +0,0 @@
-package com.micro.leaves.model;
-
-public enum LeaveStatus {
-	approved, pending, rejected
-}
-
--- LeaveService/src/main/java/com/micro/leaves/model/LeaveType.java
+++
@@ -1,6 +0,0 @@
-package com.micro.leaves.model;
-
-public enum LeaveType {
-    paid, unpaid
-}
-
